786e557b3bb51a51aef6758928bc9134c1fa0435
Clayton <cjsweeten101@gmail.com>
2017-04-30 16:09:58 -0600

    pig latin passes

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
 #write your code here
+def translate phrase
+	result = ''
+	phrase = phrase.split(' ')
+	vowels = ['a','e','i','o','u']
+	phrase.each do |word|
+		if vowels.include? word[0]
+			word += 'ay'
+		else
+			word.each_char.with_index do |char, idx|
+				if vowels.include? char
+					break
+				elsif char == 'q'
+					word = word[2..-1] + 'qu'
+					break	
+				else
+					word = word[1..-1] + char
+				end
+			end
+			word += 'ay' 
+		end
+		result += ' ' + word
+	end
+	result.strip
+end
