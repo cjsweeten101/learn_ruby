eede9ef57b5671a72e58465128f41d853cd3618a
Clayton <cjsweeten101@gmail.com>
2017-04-30 12:46:04 -0600

    Bonus tests pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -15,6 +15,22 @@ def sum arr
 	result
 end
 
-def multiply *args
-	result = args.inject(:*)
+def multiply num_1, num_2, num_3=nil
+	if(num_3 != nil)
+		result = num_1 * num_2 * num_3
+	else
+		result = num_1 * num_2
+	end
+	result
+end
+
+def power num_1, num_2
+	num_1**num_2
+end
+
+def factorial n
+	if n == 0
+		return 1
+	end
+	n * factorial(n-1)
 end
